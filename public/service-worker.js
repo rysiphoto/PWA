e315444c36114d1a71e39ef613dde184b0a81778
Ryan Siverson <rcsskier@mac.com>
2020-05-17 17:52:33 -0600

    added to the service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,6 +1,10 @@
 const FILES_TO_CACHE = [
   "/",
   "/index.html",
+  "/index.js",
+  "/routes/api.js",
+  "/routes/html.js",
+  "/indexedDb.js",
   "/models/transaction.js",
   "/icons/icon-192x192.png",
   "/icons/icon-512x512.png",
